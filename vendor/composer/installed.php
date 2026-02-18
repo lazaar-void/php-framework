f2db6293ce9c2616826a2058a22f681fb99b1de1
hamza-lzr <lazaarhamza@gmail.com>
2026-02-18 18:13:08 +0000

    set up composer.json for packagist publishing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hamza_void/my_php_framework',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '90acb3d086e86281ffce07bfebb32cce7a8a216e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hamza_void/my_php_framework' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '90acb3d086e86281ffce07bfebb32cce7a8a216e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
